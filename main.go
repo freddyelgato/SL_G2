d50bce77659ebd0efe75cfb2f7ba25bf0fd80192
freddyelgato <freddymoreirala@gmail.com>
2024-11-12 10:27:12 -0500

    feat: :sparkles: Copy my repo files GOTASK2
    The only thing I didn't copy was .github, the dockerfile, and the dockerignore

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HELLO WORLD :3\n")
+}
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
